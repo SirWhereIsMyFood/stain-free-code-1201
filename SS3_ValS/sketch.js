218933f21fcce9f2f819812d62ee24933ece4951
SirWhereIsMyFood <TunaBros11@gmail.com>
2025-03-11 15:43:13 -0400

    update in progress..

--- SS3_ValS/sketch.js
+++ SS3_ValS/sketch.js
@@ -23,12 +23,33 @@ let r2;
 let b2;
 let g2;
 
-let vendingxlocation = 190;
-let vendingylocation = 430;
+let vendingxlocation = 235; // 400 \\
+let vendingylocation = 275; // 430 \\
 let squarexlocation = 200;
 let squareylocation = 425;
+let buttonx = 305;
+let buttony = 300;
 
-let quadC = ['130', '96', '88'];
+let signxlocation = 75;  
+let signylocation = 225;
+
+let ellipseX = 75;
+let ellipseY = 225;
+
+let lineX = 60;
+let lineY = 225;
+
+let xMove = 0;
+let yMove = 0;
+
+let quadFill = ['130', '96', '88'];
+let squareFill = ['0', '0', '0', '300'];
+let vendingFill = ['10', '10', '10'];
+let panelFill = ['200', '200', '245'];
+let signFill = ['255', '255', '255', '256'];
+let buttonFill = ['226', '226', '226'];
+let ellipseFill = ['200', '0', '0'];
+let lineFill = ['0', '0', '0'];
 
 function setup()
 {
@@ -44,14 +65,50 @@ function draw ()
 
   background (r, g, b);  
 
-  fill (quadC);
+  fill (quadFill);
   quad (0, 450, 500, 450, 501, 500, 0, 500);
 
-  fill (0, 0, 0, 300);
+  // Vending Machine Build. \\
+
+  fill (squareFill);
   square (squarexlocation, squareylocation, 50);
   square (squarexlocation + 75, squareylocation, 50);
 
-  fill (10, 10, 10);
-  rect ()
+  fill (vendingFill);
+  rect (vendingxlocation, vendingylocation, width/3, height/1.5);
+
+  fill (panelFill);
+  rect (vendingxlocation, vendingylocation, width/4, height/1.75);
+
+  fill (buttonFill);
+  ellipse (buttonx, buttony, 5, 5);
+  ellipse (buttonx + 5, buttony, 5, 5);
+  ellipse (buttonx, buttony + 5, 5, 5);
+  ellipse (buttonx + 5, buttony + 5, 5, 5);
+
+  // Below I'm adding a sign on the left of the machine. \\
+
+  fill (signFill);
+
+  rect (signxlocation, signylocation, width/6, height/6);
+
+  fill (ellipseFill);
+  ellipse (ellipseX, ellipseY, width/10, height/10);
+
+  fill (lineFill);
+  stroke (0);
+  line (lineX, lineY, lineX * 1.5, lineY);
+}
+
+function mousePressed ()
+{
+
+
+}
+
+function keyPressed ()
+{
+  
+
 
 }
