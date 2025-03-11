3409f8aef7b49a73e7712ab747655c6668f2fb1c
SirWhereIsMyFood <TunaBros11@gmail.com>
2025-03-11 09:39:42 -0400

    Uploaded SS3

---
+++ SS3_ValS/sketch.js
@@ -0,0 +1,38 @@
+/*
+
+SS3 - brought to you by Val
+last modified: 3/11/25
+
+"Are you tampering with the vending machine?"
+
+I think it's time to change the scenery a little bit and go a little more zoomed in on
+the vending machine to add conditional statements and a random function to appear.
+
+I'll see what comes to mind along the way cause I'm honestly just playing around
+with this.
+
+CONTROLS:
+
+*/
+
+let r = 225;
+let g = 209;
+let b = 97;
+        // declaring variables. \\
+let r2
+let b2;
+let g2;
+
+function setup()
+{
+
+  createCanvas (500, 500); // SETUP CANVAS. \\
+
+}
+
+function draw ()
+{
+
+
+
+}
