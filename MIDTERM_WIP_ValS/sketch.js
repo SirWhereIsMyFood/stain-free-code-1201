10bc875213c1e510244d80d0abedc0acf4ab5366
SirWhereIsMyFood <TunaBros11@gmail.com>
2025-03-18 13:19:06 -0400

    going on..

--- MIDTERM_WIP_ValS/sketch.js
+++ MIDTERM_WIP_ValS/sketch.js
@@ -1,17 +1,39 @@
 /*
   MIDTERM WIP by Val
+  last modified: 3/18/25
 
   "Buying a snack from the vending machine, but it's dark souls difficulty."
 
+  For the midterm part of this course I really think I should do scenes where something
+  happens if you trigger something with a vending machine, since it's more open to possibilties
+  as far as a concept goes. I also hope to focus more on the quality of scenes first, rather
+  than the quantity of the actions that happens.
+
+  As I said in my other sketches, I'm just playing arround with things here & there.
+
+  CONTROLS:
+
 */
 
-let state = preScene;
+let state = preScene; /* I'm adding my scenes here */
 
+/* Loading fonts */
+
+
+function preload ()
+{
+
+
+
+}
 
 function setup() 
 {
 
   createCanvas (1000, 1000);
+  rectMode (CENTER);
+  textAlign (CENTER);
+  textSize (20);
 
 }
 
