43c17eb7e183a3fd64b42ea311a09539671fc522
DrAgoN009911 <TunaBros11@gmail.com>
2025-03-19 16:47:06 -0400

    !O!

--- FunWMaths_ValS/sketch.js
+++ FunWMaths_ValS/sketch.js
@@ -16,24 +16,25 @@ function setup()
 	strokeWeight(2);
 	fill(255, 255, 255, 50);
 	
-	offset = height/2; /* Offset is 250 */
-	yPrev = height/2; /* Previous Y-pos is 250 */
+	offset = height/2; /* Offset is centered y-axis */
+	yPrev = height/2; /* Previous Y-pos is centered of axis */
 }
 
 function draw() 
 {	
-	r = map(xPos, 0, width, 50, 150);
-	g = map(xPos, 0, width, 0, 255);
-	b = map(xPos, 0, width, 255, 0);
-	
-	background(r, g, b, 5); 
+	r = map(xPos, 0, width, 50, 150); /* red is (x-pos, start at 0, stops at width, starts second value at 50, stops at 150)  */ 
+	g = map(xPos, 0, width, 0, 255); /* blue is (x-pos, start at 0, stops at width, starts second value at 0, stops at 255) */
+	b = map(xPos, 0, width, 255, 0); /* green is (x-pos, start at 0, stops at width, starts second value at 255, stops at 0) */
 
-	lerpAmount = map(mouseY, 0, height, 0.2, 0.9);
+	background(r, g, b, 5); /* Background is constantly shifting in color values based on map function. */
+
+	lerpAmount = map(mouseY, 0, height, 0.2, 0.9); /* Although I don't understand it completely, it creates the movement of the circle path. */
 	
-  yPos = amplitude * sin(xPos * frequency) + offset;
+  yPos = amplitude * sin(xPos * frequency) + offset; /* The y-pos is the amplitude multiplied by sin, and x-pos is multiplied by the
+  frequency of how often it happens + the current value (offset) */ 
 	
-	yPos = lerp(yPrev, yPos, lerpAmount);
-	xPos = lerp(xPrev, xPos, lerpAmount);
+	yPos = lerp(yPrev, yPos, lerpAmount); /* Y-pos */
+	xPos = lerp(xPrev, xPos, lerpAmount); /* */
 	
 	circle(xPos, yPos, 50);
 	
