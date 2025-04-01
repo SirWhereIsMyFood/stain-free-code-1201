6a0ac26fbae1e20a7838b02ea5a355630986a442
SirWhereIsMyFood <78876655+SirWhereIsMyFood@users.noreply.github.com>
2025-04-01 15:51:51 -0400

    v2v2

--- Midterm-V2-ValS/sketch.js
+++ Midterm-V2-ValS/sketch.js
@@ -4,7 +4,19 @@ last modified 04/01/24 (april fools)
 
 " Buying a snack from the vending machine, but it's kind of a whacky adventure. "
 
-For my final iteration 
+For my final iteration, I didn't just copy and pasted then added changes to it, but I'm just
+stating what I really wanted to go for. At least in here I did wanted to fix the erros
+that wasn't functioning the way I would want it to, and then see what I can really add from
+there + feedback from peers on last weeks review.
 
+As I said in all sketches, I'm really just playing around with things here and there
+
+SOURCES:
+  monogram fonts - https://datagoblin.itch.io/monogram
+  fade animation text - https://editor.p5js.org/remarkability/sketches/rtM08miUD
+  clcking on objects - The Coding Train "7.6: Clicking on Objects - p5.js Tutorial" URL: https://www.youtube.com/watch?v=DEHsr4XicN8&t=265s
+  Session 05 - MTEC 1201
 */
+
+
 
