3545968fc02d3bd7358e04866e1517c041c40694
SirWhereIsMyFood <78876655+SirWhereIsMyFood@users.noreply.github.com>
2025-04-01 15:46:50 -0400

    Yup.

--- Midterm-V2-ValS/sketch.js
+++ Midterm-V2-ValS/sketch.js
@@ -1,7 +1,10 @@
-function setup() {
-  createCanvas(400, 400);
-}
+/*
+MIDTERM Iteration by Val
+last modified 04/01/24 (april fools)
 
-function draw() {
-  background(220);
-}
+" Buying a snack from the vending machine, but it's kind of a whacky adventure. "
+
+For my final iteration 
+
+*/
+
