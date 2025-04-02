8f67b565918e847b63a1a00527cd4ca0b440bd23
SirWhereIsMyFood <78876655+SirWhereIsMyFood@users.noreply.github.com>
2025-04-01 22:36:12 -0400

    press s p a c e

--- Midterm-V2-ValS/sketch.js
+++ Midterm-V2-ValS/sketch.js
@@ -18,7 +18,7 @@ SOURCES:
   Session 05 - MTEC 1201
 */
 
-let state = "mainMenu"; /* Here is where I'm throwing my scenes in */
+let state = "main menu"; /* Here is where I'm throwing my scenes in */
 let venddrink = 0; /* t/f statements */
 let vendfood = 0;
 
@@ -46,3 +46,158 @@ let fade = 1;
 let strokefade = 5
 
 let currentTime;
+
+let vendingxlocation = 500;
+let vendingylocation = 575;
+let squarexlocation = 425;
+let squareylocation = 800;
+let buttonx = 305;
+let buttony = 300;
+let windowx = 200;
+let windowy = 200;
+
+/* grouped ellipse to turn into "spawtify" */
+let spawtifyX = 75;
+let spawtifyY = 225;
+let lineX
+let lineY
+
+let quadFill = [130, 96, 88];
+let squareFill = [0, 0, 0, 300];
+let vendingFill = [10, 10, 10];
+let panelFill = [200, 200, 245];
+let signFill = [255, 255, 255, 256];
+let buttonFill = [226, 226, 226];
+let ellipseFill = [200, 0, 0];
+let lineFill = [0, 0, 0];
+let slotFill = [200, 200, 245];
+
+function preload()
+{
+
+  myFont = loadFont ('font/monogram.ttf');
+
+  food[0] = loadImage ("img/SPAM.png");
+  food[1] = loadImage ("img/Tunacan.png");
+  food[2] = loadImage ("img/onigiri.png");
+  bill = loadImage ("img/PNGCipher.png");
+  drink[0] = loadImage ("img/BottledWater.png");
+  drink[1] = loadImage ("img/gatorade.png");
+  drink[2] = loadImage ("img/drpepper.png");
+  hand = loadImage ("img/HandWater.png");
+  drink[3] = loadImage ("img/BottledPoison.png");
+
+  /* displayed images that are movable above
+  and images below will be for display only */
+
+  dispFOOD[0] = loadImage ("img/SPAM.png");
+  dispFOOD[1] = loadImage ("img/Tunacan.png");
+  dispFOOD[2] = loadImage ("img/onigiri.png");
+  dispDRINK[0] = loadImage ("img/BottledWater.png");
+  dispDRINK[1] = loadImage ("img/gatorade.png");
+  dispDRINK[2] = loadImage ("img/drpepper.png");
+  dispDRINK[3] = loadImage ("img/question.png");
+  
+}
+
+function setup() 
+{
+
+  createCanvas (1000, 1000);
+  rectMode (CENTER);
+  textAlign (CENTER);
+  imageMode (CENTER);
+
+}
+
+function draw() 
+{
+  if (state == "main menu")
+  {
+    mainMenu ();
+    print (state);
+  }
+  else if (state == "game")
+  {
+    game ();
+    print (state);
+  }
+  else if (state == "scene over")
+  {
+    sceneOver ();
+    print (state);
+  } 
+
+  print (state);
+
+}
+
+/* The starting point for the game, has a cooldown time to procees. 
+Scene leads to explanation of the sypnosis and controls. */
+function mainMenu()
+{
+background (r, g, b);
+  textSize (100);
+  textFont (myFont);
+  text ('WELCOME, PRESS THE SPACE BAR TO PROCEED.', width/2, height/2, width/2, height/2);
+
+  if (key == " ")
+  {
+    /* timer variables */
+  let currentTime = 0;
+  let timer1 = 8000 ;
+  let timer2 = 10000;
+
+  currentTime = millis();
+
+  background (r, g, b);
+  textSize (35);
+  textFont (myFont);
+  fill (opacity);
+  text ("You're just trying to get a snack from the vending machine, in the cafeteria. But why is there a funny feeling about this? And why are you here alone? Couldn't you just bring your own food?", 500, 300, width/2, height/2);
+
+  textSize (50);
+  stroke(0);
+  strokeWeight (strokefade);
+  fill (opacity);
+  text ("C O N T R O L S", width/2, height/2, width/2, height/4);
+  text ("1 - F O O D", width/2 - 150, height/2 + 150, width/2 - 150, height/4 + 150);
+  text ("2 - D R I N K S", width/2 + 150, height/2 + 150, width/2 - 150, height/4 + 150);
+  noStroke();
+  opacity = opacity + fade;
+
+  if (currentTime > timer2)
+  {
+    text ("P R E S S  S P A C E  T O  P R O C E E D", width/2, height/2 + 400, width/2, height/4 + 200);
+  }
+  else if (currentTime > timer1)
+  {
+    if (opacity > 255)
+    {
+      fade = fade - 0.5;
+      strokefade = strokefade - 1;
+    }
+  }
+
+  print ("currentTime =" + currentTime);
+  }
+  else if (key == " " && currentTime > timer2)
+  {
+    game ();
+  }
+}
+
+function game ()
+{
+  background (r, g, b);
+}
+
+function keyPressed ()
+{
+  print ("Key pressed: " + key);
+  if (key == " " && state == "main menu")
+  {
+    background(0);
+    background(r, g, b);
+  }
+}
