29bef712bb7a358eefda237f049015be917bbcb2
SirWhereIsMyFood <78876655+SirWhereIsMyFood@users.noreply.github.com>
2025-04-04 21:32:50 -0400

    !i

--- SS5_ValS/val.js
+++ SS5_ValS/val.js
@@ -4,9 +4,20 @@ SHORT STUDY 5
 BROUGHT TO YOU BY VAL S.
 Last Modified: (04.04.25)
 
+" Why is my food acting up?? "
+
+Sypnosis:
 Soo.. after the midterm I do want to continue the whole vending machine theme, however I
 want to brach off from that a bit and focus on this whole scenery but from another angle
 testing out the for loops or even creating nested loops.
 
 */
 
+function setup ()
+{
+
+
+
+
+
+}
