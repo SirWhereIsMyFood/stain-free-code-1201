55bd72ed6e05b9dab0a301b4e10f5ea1c9d9227b
SirWhereIsMyFood <78876655+SirWhereIsMyFood@users.noreply.github.com>
2025-04-06 16:38:35 -0400

    not what I hoped but we are getting there

--- SS5_ValS/val.js
+++ SS5_ValS/val.js
@@ -17,13 +17,14 @@ let MISC = [];
 
 let WHATISMISC = 0;
 
-function preLoad ()
+function preload ()
 {
+  /*
   MISC [0] = loadImage ('imgs/choco.png');
   MISC [1] = loadImage ('imgs/snack1.png');
   MISC [2] = loadImage ('imgs/snack2.png');
   MISC [3] = loadImage ('imgs/snack3.png');
-
+*/
 }
 
 function setup ()
@@ -33,20 +34,20 @@ function setup ()
   rectMode (CENTER);
   imageMode (CENTER);
   background (100);
- //colorMode (HSL);
+ colorMode (HSL);
 
 }
 
 function draw ()
 {
 
-  let x = random(250, 750);
+  //let x = random(250, 750);
   let gravity = 400;
-  let imgs = 10;
+  let imgs = 25;
 
- // let c = color (220, 225, 97);
-  //background (c);
-  background (220, 225, 97);
+  let c = color (220, 225, 97);
+  background (c);
+  //background (220, 225, 97);
   noFill();
   stroke (0);
   strokeWeight (10);
@@ -56,28 +57,37 @@ function draw ()
   //noStroke ();
   
   /* make sure mouse is within boundaries of window for inanimate object to fall */
-  for (let i = 400; i <= 725; i += imgs)
+  for (let x = random(250, 750); x <= 725; x += imgs)
   //if (mouseX > 250 && mouseX < 750 && mouseY > 400 && mouseY < 700)
   {
     //for (let i = 400; i <= 725; i += imgs)
       if (mouseX > 250 && mouseX < 750 && mouseY > 400 && mouseY < 700)
     {
       WHATISMISC = int(random(MISC.length))
-      image (WHATISMISC, x, gravity, imgs, imgs);
+      //image (WHATISMISC, x, gravity, imgs, imgs);
       fill (random(100, 200), 220, 97);
-      ellipse (x, gravity, imgs, imgs)
+      ellipse (x, gravity, imgs, imgs);
 
       strokeWeight (5);
     quad (250, 250, 750, 250, 725, 400, 275, 400); 
     quad (250, 750, 750, 750, 725, 700, 275, 700);
+      
+      gravity += random(0.1, 1);
+
+    if (gravity >= 725)
+    {
+      gravity =- imgs
+    }
     }
 
+    /*
     gravity += random(1, 10);
 
     if (gravity >= 725)
     {
       gravity =- imgs
     }
+    */
   
   }
 
