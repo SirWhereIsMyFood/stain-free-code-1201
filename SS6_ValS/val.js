d8aedca9b247eb698d17e4a6a728902b8e124ec9
SirWhereIsMyFood <78876655+SirWhereIsMyFood@users.noreply.github.com>
2025-04-22 12:36:31 -0400

    webgl illuminating time with spheres

--- SS6_ValS/val.js
+++ SS6_ValS/val.js
@@ -11,5 +11,30 @@ that I had in mind.
 " Just enjoy the show.. "
 
 No keys need to be pressed, sit back and relax for this time around.
+
 */
+
+/* ARRAYS */
+let arrayVal = [];
+let spheres = [];
+
+/* TIMER VAR'S */
+let ctime = 0;
+let ptime = 0;
+let timer = 1500;
+
+function setup ()
+{
+
+    createCanvas (1000, 1000, WEBGL);
+
+}
+
+function draw ()
+{
+
+    ctime = millis();
+    
+
+}
 
