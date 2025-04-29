9f712ade2b1e54d8e1d8bcd07b61fbbeb3bb5a5a
SirWhereIsMyFood <78876655+SirWhereIsMyFood@users.noreply.github.com>
2025-04-29 01:06:45 -0400

    class vending

--- SS7_ValS/val.js
+++ SS7_ValS/val.js
@@ -1,7 +1,34 @@
-function setup() {
-  createCanvas(400, 400);
+/*
+
+SS7
+by Val S.
+(04/27/25)
+
+For this sketch, I'm likely making a class of a vending machine,
+seeing with how I'm able to manage this considering the amount of primitive
+shapes it takes to make one (scaling, size, location).
+
+*/
+
+let vending1;
+let vending2;
+
+function setup ()
+{
+
+  createCanvas (windowWidth, windowHeight);
+
 }
 
-function draw() {
-  background(220);
+function draw ()
+{
+
+
+
+}
+
+class Vending
+{
+
+  
 }
