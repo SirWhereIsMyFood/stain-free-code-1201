79569cef64558262480e7bd36f3ad39ec74c418f
SirWhereIsMyFood <78876655+SirWhereIsMyFood@users.noreply.github.com>
2025-05-02 22:09:24 -0400

    excuse me??

--- FINAL_WIP_ValS/val.js
+++ FINAL_WIP_ValS/val.js
@@ -22,3 +22,12 @@ haven't really done yet.
 CONTROLS:
 
 */
+
+function setup() {
+  noCanvas();
+
+  // Create the video capture.
+  createCapture(VIDEO);
+
+  describe('A video stream from the webcam.');
+}
